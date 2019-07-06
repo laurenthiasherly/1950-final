7b849a5644738d0a184ce2efb7b172c87108b1de
Laurenthia <laurenthiasherly@gmail.com>
2019-07-05 22:12:13 -0700

    First file set up for the project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>FooCorp - Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+</head>
+
+<body>
+
+	
+	<script src="scripts/menu.js"></script>
+</body>
+</html>
